0e4725a3bc3d48fbb57ed0be2601001081939e2f
Marco <marco@MacBook-Pro-de-Marco.local>
2022-02-01 19:20:46 -0300

    Semana 2 - 2

--- Aula01/ex-variaveis.js
+++ Aula01/ex-variaveis.js
@@ -3,7 +3,20 @@ let peso = 72;
 let altura = 1.72;
 let imc = peso / Math.pow(altura, 2);
 
-console.log(imc);
+console.log(imc.toFixed(2));
+
+
+// Fahrenheit x Celsius
+let celsius = 30;
+let fahrenheit = celsius * 1.8 + 32;
+
+console.log(fahrenheit.toFixed(2));
+
+// Desconto do tênis
+
+
+
+
 
 
 
